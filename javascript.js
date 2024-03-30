7f75184ca1915f9892ddff2e091d8047ee78547c
Ananya Dhiman <ananya.dhiman@somaiya.edu>
2024-03-30 18:26:40 +0530

    game 2 play with buttons

--- javascript.js
+++ javascript.js
@@ -1,35 +1,107 @@
 function getComputerChoice(){
-    let arr=["Rock","Scissors","Paper"];
-    const random = Math.floor(Math.random() * arr.length);
-    return arr[random];
-} 
+  let arr=["Rock","Scissors","Paper"];
+  const random = Math.floor(Math.random() * arr.length);
+  return arr[random];
+}
+
 
 function round(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-        return "'It's a tie!";
-      }
-    else if (
-        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
-        (playerSelection === 'Paper' && computerSelection === 'Rock')
-      ) {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection + '!';
-      }
-      else {
-        return 'You lose! ' + computerSelection + ' beats ' + playerSelection + '!';
-      };
 
+
+
+  
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  const res=document.querySelector("#Result");
+  const text=document.createElement("p");
+  text.classList.add("text");
+  const nump=document.createElement("div");
+  const numc=document.createElement("div");
+  const counter=document.querySelector(".count");
+  nump.classList.add("nump");
+  numc.classList.add("numc");
+  
+  if (playerSelection === computerSelection) {
+    text.textContent="'It's a tie!";
+    
     }
+  else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper') ||
+      (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+      playerCount=playerCount+1;
 
-  function playGame(n){
-    for (let i = 0; i < n; i++) {
-        let computerSelection=getComputerChoice();
-        let playerSelection=(prompt("Enter Choice: "));
-        console.log(round(playerSelection, computerSelection));
+      text.textContent='You win! ' + playerSelection + ' beats ' + computerSelection + '!';
 
+      }
+    else {
+      computerCount=computerCount+1;
+
+      text.textContent= 'You lose! ' + computerSelection + ' beats ' + playerSelection + '!';
+      
+    };
+   
+    res.setAttribute("style", "color:#003153; font-size:22px; font-weight:bold; display:flex ; flex-flow:column wrap; align-items:center; background-color:#94e5ff; margin:0px 700px; border-radius:50px");
+ 
+    counter.textContent = '';
+    counter.textContent= `Player: ${playerCount}               |              Computer: ${computerCount}`;
+   
+    let r=checkWinner();
+    if(r===true){
+      playerCount = 0;
+      computerCount = 0;
+      const counter = document.querySelector(".count");
+      counter.textContent = `Player: 0              |              Computer: 0`;
+   
+      setTimeout(function () {
+        const res= document.querySelector("#Result");
+        res.textContent="New Game";
+      
+      }, 1000);
+      
+      
     }
+    
+    
+  }
+
+
+function playGame(){
+
+  let computerSelection=null;
+  let playerSelection='';
+  
+  
+ 
+  const con= document.querySelectorAll("button");
+
+    con.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        playerSelection = event.target.id;
+        computerSelection=getComputerChoice();
+        
+        round(playerSelection, computerSelection);
+        
+  });
+});
 }
-let n=parseInt(prompt("Enter Number of Rounds: "));
-playGame(n);
+
+function checkWinner() {
+  if (playerCount === 5 || computerCount === 5) {
+    const winner =
+      playerCount === 5
+        ? "You win the game! Congratulations!"
+        : "Computer wins the game! Try again next time!";
+    alert(winner);
+
+    return true;
+  }
+  return false;
+}
+let playerCount=0;
+let computerCount=0;
+
+playGame();
+
+
